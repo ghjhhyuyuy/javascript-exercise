cbb1c4ccc10142019b33281533c8644ccc59649e
yourname <your@email.com>
2020-08-10 23:27:47 +0800

    [WangZuowen]feat:finish astnc promise index.js

--- 5-async/2-promise/index.js
+++ 5-async/2-promise/index.js
@@ -3,7 +3,20 @@ function fetchData(url) {
     const xhr = new XMLHttpRequest();
     // <-- start
     // TODO 22: 通过Promise实现异步请求
-
+    xhr.open('POST', url, true);
+    xhr.setRequestHeader('content-type', 'application/json');
+    xhr.onload = function() {
+      if (xhr.status >= 200 && xhr.status <= 299) {
+        resolve({ responseText: xhr.responseText, statusText: xhr.statusText });
+      } else {
+        const error = new Error(xhr.responseText);
+        error.responseText = xhr.responseText;
+        error.statusText = xhr.statusText;
+        reject(error);
+      }
+    };
+    const payload = { name: 'hello' };
+    xhr.send(JSON.stringify(payload));
     // end -->
   });
 }
