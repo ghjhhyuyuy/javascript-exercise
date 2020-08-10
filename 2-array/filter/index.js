2bad8069b96f04375ccbe0d1240e743d020ff113
yourname <your@email.com>
2020-08-10 20:46:50 +0800

    [WangZuowen]feat:Finish index.js.

--- 2-array/filter/index.js
+++ 2-array/filter/index.js
@@ -1,9 +1,11 @@
 function chooseMultiplesOfThree(collection) {
   // TODO 1: 在这里写实现代码
+  return collection.filter(num => num % 3 === 0);
 }
 
 function chooseNoRepeatNumber(collection) {
   // TODO 2: 在这里写实现代码
+  return collection.filter((num, index) => collection.indexOf(num, 0) === index);
 }
 
 export { chooseMultiplesOfThree, chooseNoRepeatNumber };
