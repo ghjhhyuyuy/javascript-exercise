b9cb5c6e6e8c7ec8726868b2321e4d3ff52afd1c
yourname <your@email.com>
2020-08-10 23:17:58 +0800

    [WangZuowen]feat:finish astnc xhr index.js

--- 5-async/1-xhr/index.js
+++ 5-async/1-xhr/index.js
@@ -2,7 +2,17 @@ function fetchData(url, successCallback, errorCallback) {
   const xhr = new XMLHttpRequest();
   // <-- start
   // TODO 21: 通过XMLHttpRequest实现异步请求
-
+  xhr.open('POST', url);
+  xhr.setRequestHeader('content-type', 'application/json');
+  xhr.onload = function() {
+    if (xhr.status >= 200 && xhr.status <= 299) {
+      successCallback({ responseText: xhr.responseText, statusText: xhr.statusText });
+    } else {
+      errorCallback({ responseText: xhr.responseText, statusText: xhr.statusText });
+    }
+  };
+  const payload = { name: 'hello' };
+  xhr.send(JSON.stringify(payload));
   // end -->
 }
 
