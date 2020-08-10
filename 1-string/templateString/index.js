73bc4fb16b73800375b12230674567e7ace6be6d
yourname <your@email.com>
2020-08-10 21:19:46 +0800

    [WangZuowen]feat:Finish string templateString index.js.

--- 1-string/templateString/index.js
+++ 1-string/templateString/index.js
@@ -7,4 +7,7 @@ function getScore(id) {
 
 export default function getScoreOutput(person) {
   // TODO 8: 在这里写实现代码
+  const score = getScore(person.id);
+  const name = person.lastName + person.firstName;
+  return `你好，${name}！你的考试成绩为${score}分`;
 }
